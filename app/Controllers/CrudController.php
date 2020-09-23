402b7cb2a1f3117d14d67e34a7feb469a3cbfa47
fharavena <fharavena@gmail.com>
2020-09-23 18:39:13 -0300

    Update CrudController.php

--- app/Controllers/CrudController.php
+++ app/Controllers/CrudController.php
@@ -9,6 +9,13 @@ use App\Models\CrudModel;
 
 class CrudController extends Controller
 {
+    public function options(): Response
+    {
+        return $this->response->setHeader('Access-Control-Allow-Origin', '*') //for allow any domain, insecure
+            ->setHeader('Access-Control-Allow-Headers', '*') //for allow any headers, insecure
+            ->setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE') //method allowed
+            ->setStatusCode(200); //status code
+    }
     
     public function index()
     {
