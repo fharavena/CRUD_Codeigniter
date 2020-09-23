8056248aec132dff0dbc960110b93ede19895c93
fharavena <fharavena@gmail.com>
2020-09-23 19:02:37 -0300

    Update CrudController.php

--- app/Controllers/CrudController.php
+++ app/Controllers/CrudController.php
@@ -2,7 +2,15 @@
 
 namespace App\Controllers;
 
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 
 use CodeIgniter\Controller;
 use CodeIgniter\HTTP\RequestInterface;
